8b78e4415d6d1aefa74ae053758774e90f2375d5
MasyumaroGames <masyumarogames@gmail.com>
2025-09-07 15:59:32 +0900

    Initial commit

--- src/main/java/com/MasyumaroGames/MasyuMod/item/tool/MasyuModTiers.java
+++ src/main/java/com/MasyumaroGames/MasyuMod/item/tool/MasyuModTiers.java
@@ -1,16 +1,27 @@
 package com.MasyumaroGames.MasyuMod.item.tool;
 
+import com.MasyumaroGames.MasyuMod.main.MasyuMod;
 import com.MasyumaroGames.MasyuMod.regi.MasyuModItems;
 import com.MasyumaroGames.MasyuMod.regi.MasyuModTags;
-import net.minecraft.client.Minecraft;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.MinecartItem;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Tiers;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraftforge.common.ForgeTier;
+import net.minecraftforge.common.TierSortingRegistry;
+
+import java.util.List;
 
 public class MasyuModTiers {
 
     public static final ForgeTier COMBINE = new ForgeTier(5,2300,8.0F,3.0F,12, MasyuModTags.Blocks.NEEDS_COMBINE_TOOL,()-> Ingredient.of(MasyuModItems.COMBINE_INGOT.get()));
 
     public static final ForgeTier MASYU = new ForgeTier(6,5000,20.0F,6.0F,22, MasyuModTags.Blocks.NEEDS_MASYU_TOOL,()-> Ingredient.of(MasyuModItems.MASYU_INGOT.get()));
+
+    static {
+        TierSortingRegistry.registerTier(MASYU,new ResourceLocation(MasyuMod.MOD_ID,"masyu"), List.of(COMBINE),List.of());
+    }
+
+    static {
+        TierSortingRegistry.registerTier(COMBINE,new ResourceLocation(MasyuMod.MOD_ID,"combine"), List.of(Tiers.NETHERITE),List.of(MASYU));
+    }
 }
